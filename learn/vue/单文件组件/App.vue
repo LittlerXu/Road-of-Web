4b608ecfdbc689ae171bf9c6ccb0c3a7772d0809
LittlerXu <2281482223@qq.com>
2023-02-13 14:17:08 +0800

    just push

---
+++ learn/vue/单文件组件/App.vue
@@ -0,0 +1,26 @@
+<template>
+    <!-- 编写组件标签 -->
+   <div>
+    <School></School>
+    <Student></Student>
+   </div>
+</template>
+
+<script>
+//导包
+import School from './School.vue'
+import Student from './Student.vue'
+
+export default {
+    name: 'App',
+    //注册组件
+    components: {
+        School,
+        Student
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ learn/vue/单文件组件/School.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="demo">
+    <h2>学校名称:{{ name }}</h2>
+    <h2>学校地址:{{ address }}</h2>
+    <button @click="alertSchoolName">点我提示学校名称</button>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "School",
+    data() {
+        return {
+            name: '贵州大学',
+            address: '花溪区'
+        }
+    },
+    methods: {
+        alertSchoolName() {
+            alert(this.name)
+        }
+    }
+}
+</script>
+
+<style>
+    .demo {
+        background-color: orange;
+    }
+</style>
