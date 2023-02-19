35b363e45e1c101de84fe533f57594bbfcddd215
LittlerXu <2281482223@qq.com>
2023-02-19 16:24:59 +0800

    just push

---
+++ learn/vue/vue_test/03_src_props配置/App.vue
@@ -0,0 +1,15 @@
+<template>
+	<div>
+		<!-- 给属性前面加":"可以使传入的"18"为数字类型而非字符串类型,因为此时""中的内容视为表达式 -->
+		<Student name="李四" sex="女" :age="18"/>
+	</div>
+</template>
+
+<script>
+	import Student from './components/Student'
+
+	export default {
+		name:'App',
+		components:{Student}
+	}
+</script>
---
+++ learn/vue/vue_test/03_src_props配置/components/Student.vue
@@ -0,0 +1,53 @@
+<template>
+	<div>
+		<h1>{{msg}}</h1>
+		<h2>学生姓名：{{name}}</h2>
+		<h2>学生性别：{{sex}}</h2>
+		<h2>学生年龄：{{myAge+1}}</h2>
+		<button @click="updateAge">尝试修改收到的年龄</button>
+	</div>
+</template>
+
+<script>
+	export default {
+		name:'Student',
+		data() {
+			console.log(this)
+			return {
+				msg:'我是一个尚硅谷的学生',
+				myAge: this.age, //将props中的属性赋值给自定义的属性,然后对这个自定义属性进行操作可以间接达到修改props中属性的目的
+				name: '张三' //props中的属性会覆盖data中的同名属性
+			}
+		},
+		methods: {
+			updateAge(){
+				this.myAge++
+			}
+		},
+		//简单声明接收
+		// props:['name','age','sex']
+
+		//接收的同时对数据进行类型限制
+		/* props:{
+			name:String,
+			age:Number,
+			sex:String
+		} */
+
+		//接收的同时对数据：进行类型限制+默认值的指定+必要性的限制
+		props:{
+			name:{
+				type:String, //name的类型是字符串
+				required:true, //name是必要的
+			},
+			age:{
+				type:Number,
+				default:99 //默认值
+			},
+			sex:{
+				type:String,
+				required:true
+			}
+		}
+	}
+</script>
